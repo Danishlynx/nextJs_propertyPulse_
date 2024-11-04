0e99509a2789e2ceaec4a67cbb4328464f17f72a
Danish Ali <danishlynx@gmail.com>
2024-11-04 23:55:14 +0000

    setting amenities, specific properties of website

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,6 @@
 import connectDB from '@/app/config/database';
 import Property from '@/app/models/Property';
+import PropertyDetails from '@/components/PropertyDetails';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
@@ -22,7 +23,14 @@ const PropertyPage = async ({ params }) => {
           </Link>
         </div>
       </section>
-      <section>{property.name}</section>;
+      <section className='bg-blue-50'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            {/* Property Info */}
+            <PropertyDetails property={property} />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
