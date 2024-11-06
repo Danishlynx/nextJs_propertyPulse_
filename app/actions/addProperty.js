30494414490019e30516ddf22d2437942d11a889
Danish Ali <danishlynx@gmail.com>
2024-11-06 15:54:26 +0000

    add property data

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,6 +1,21 @@
 'use server';
+import connectDB from '../config/database';
+import Property from '../models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 async function addProperty(formData) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User ID is required');
+  }
+
+  const { userId } = sessionUser;
+
   // Access all values from amenities and images
   const amenities = formData.getAll('amenities');
   const images = formData
@@ -9,6 +24,7 @@ async function addProperty(formData) {
     .map((image) => image.name);
 
   const propertyData = {
+    owner: userId,
     type: formData.get('type'),
     name: formData.get('name'),
     description: formData.get('description'),
@@ -18,7 +34,7 @@ async function addProperty(formData) {
       state: formData.get('location.state'),
       zipcode: formData.get('location.zipcode'),
     },
-    name: formData.get('name'),
+    beds: formData.get('beds'),
     baths: formData.get('baths'),
     square_feet: formData.get('square_feet'),
     amenities,
@@ -34,7 +50,11 @@ async function addProperty(formData) {
     },
     images,
   };
-  console.log(propertyData);
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath('/', 'layout');
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
